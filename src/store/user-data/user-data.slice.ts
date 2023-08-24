30430f8287a40a63b39b6f43baa08b682d43398a
Эдуард <krav_4@mail.ru>
2023-08-24 10:10:43 +0300

    Корректировки

--- src/store/user-data/user-data.slice.ts
+++ src/store/user-data/user-data.slice.ts
@@ -1,7 +1,7 @@
 import {createSlice} from '@reduxjs/toolkit';
 import {AuthorizationStatus, RequestStatus, NameSpace} from '../../const';
-import {checkAuthAction, loginAction, logoutAction} from '../api-actions';
 import {UserData} from '../../types/state-types';
+import {checkAuthAction, loginAction, logoutAction} from '../api-actions';
 
 const initialState: UserData = {
   user: null,
